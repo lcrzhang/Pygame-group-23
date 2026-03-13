de57100ae9b64e285f8b24277c228e05e669b53a
lcrzhang <uvadriveleo@gmail.com>
2026-03-13 12:42:22 +0100

    random color en shape toegevoegd

--- Projectile.py
+++ Projectile.py
@@ -1,13 +1,15 @@
 import pygame
+import random
 
 class Projectile:
-    color = (255, 0, 0)
     
     def __init__(self, position, speed, size):
         self.position = pygame.Vector2(position)
         self.speed = pygame.Vector2(speed)
         self.width = size
         self.height = size
+        self.color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+        self.shape = random.choice(['rect', 'circle', 'triangle'])
 
     def update(self, world_size):
         self.position += self.speed
@@ -27,5 +29,17 @@ class Projectile:
         return pygame.Rect(self.position.x, self.position.y, self.width, self.height)
 
     def draw(self, surface):
-        rect = self.get_rect()
-        pygame.draw.rect(surface, Projectile.color, rect)
+        if self.shape == 'rect':
+            rect = self.get_rect()
+            pygame.draw.rect(surface, self.color, rect)
+        elif self.shape == 'circle':
+            radius = self.width // 2
+            center = (int(self.position.x + radius), int(self.position.y + radius))
+            pygame.draw.circle(surface, self.color, center, radius)
+        elif self.shape == 'triangle':
+            points = [
+                (self.position.x + self.width / 2, self.position.y),
+                (self.position.x, self.position.y + self.height),
+                (self.position.x + self.width, self.position.y + self.height)
+            ]
+            pygame.draw.polygon(surface, self.color, points)
